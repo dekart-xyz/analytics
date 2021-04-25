8be28ce617d5473f6dce3e9aa977cbf6d34d6169
Vladi Bilonenko <bilonenko.v@gmail.com>
2021-04-25 09:59:13 +0200

    Analytics

--- notebooks/reports-queries.sql
+++ notebooks/reports-queries.sql
@@ -3,13 +3,15 @@ select
     author_email,
     title,
     archived,
-    reports.created_at as queries_created_at,
-    reports.updated_at as queries_updated_at,
+    reports.created_at as reports_created_at,
+    reports.updated_at as reports_updated_at,
     query_text,
     job_status,
+    job_error,
     total_rows,
     bytes_processed,
     result_size,
     queries.created_at as queries_created_at,
     queries.updated_at as queries_updated_at
 from queries left join reports on queries.report_id = reports.id
+order by reports.created_at
